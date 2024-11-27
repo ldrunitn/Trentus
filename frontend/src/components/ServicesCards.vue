a11c344a2c83746eb08171903c766c99a2f3c472
Josip <contegiuseppe2015@gmail.com>
2024-11-27 11:17:29 +0100

    merge feature

--- frontend/src/components/ServicesCards.vue
+++ frontend/src/components/ServicesCards.vue
@@ -4,9 +4,10 @@
 
 <template>
 
-<div class="bg-white rounded-lg shadow-md p-8 max-w-45 mx-8">
+<div class="bg-white rounded-lg shadow-md p-16 max-w-45 mx-8">
     <h3 class="text-lg font-bold">Servizio 1</h3>
     <p class="text-gray-600">Descrizione servizio 1</p>
+    <p class="text-green-500 text-right leading-loose">status</p>
 </div>
 
 
