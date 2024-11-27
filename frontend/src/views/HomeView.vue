db9a7b235a5418dc7e640dd05a8e1f7e1bae5631
Josip <contegiuseppe2015@gmail.com>
2024-11-27 08:42:21 +0100

    frontend

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+<!-- scheletro homepage: -->
+  <!-- homecards (servizi) (v-for con tutte le api dei servizi) -->
+  <!-- navbar superiore  (abbastanza statico, basta che reindirizzi dove deve)-->
+  <!-- sidebars (a destra bisogna fare un v-for con dellle mini schede api, a sinistra dei paragrafi che si possano collassare )  -->
+</template>
