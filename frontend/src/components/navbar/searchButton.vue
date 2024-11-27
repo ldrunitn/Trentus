023b3b2ef6ca4b70d91e4accb09bcbac31a35f67
Ricxel <giovannisbalchiero04ita@gmail.com>
2024-11-27 13:37:20 +0100

    navbar iniziata

---
+++ frontend/src/components/navbar/searchButton.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="flex items-center border border-gray-300 rounded-lg p-2">
+    <input type="text" placeholder="Cerca" class="flex-grow bg-gray-200 text-gray-600 outline-none p-2 rounded-l-lg" />
+    <button class="p-2 bg-gray-200 rounded-r-lg">
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 16l4 4m0 0l-4-4m4 4l-4-4m-4 0a6 6 0 1 0-12 0 6 6 0 0 0 12 0z" />
+        </svg>
+    </button>
+</div>
+</template>
+<script>
+export default{
+
+}
+</script>
