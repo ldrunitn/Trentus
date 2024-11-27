242ee9a581a46b140d2e3ccaf7d2f67f07c1f11d
Josip <contegiuseppe2015@gmail.com>
2024-11-27 08:42:21 +0100

    frontend

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template> 
+    <nav class="bg-gray-500">
+        <div class="container mx-auto flex justify-between items-center p-4">
+            <router-link to="/" class="text-red font-bold">Home</router-link>
+            <router-link to="/preferiti" class="text-white font-bold">Preferiti</router-link>
+            <router-link to="/impostazioni" class="text-white font-bold">Impostazioni</router-link>
+            <router-link to="/profilo" class="text-white font-bold">Profilo</router-link>
+        </div>
+    </nav>
+</template>
