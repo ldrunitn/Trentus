50907f02d4a6f2b010dd757a49688be719512096
Ricxel <giovannisbalchiero04ita@gmail.com>
2024-11-27 20:39:51 +0100

    Filtro ricerca navbar

--- frontend/src/components/navbar/searchButton.vue
+++ frontend/src/components/navbar/searchButton.vue
@@ -1,15 +1,81 @@
 <template>
-<div class="flex items-center border border-gray-300 rounded-lg p-2">
-    <input type="text" placeholder="Cerca" class="flex-grow bg-gray-200 text-gray-600 outline-none p-2 rounded-l-lg" />
-    <button class="p-2 bg-gray-200 rounded-r-lg">
-        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 16l4 4m0 0l-4-4m4 4l-4-4m-4 0a6 6 0 1 0-12 0 6 6 0 0 0 12 0z" />
-        </svg>
+<div class="flex items-center border border-gray-500 rounded-3xl px-4 h-16 w-96 mr-4" id="search-div">
+    <input v-model="searchQuery" type="text" placeholder="Cerca" id="search-input" class="flex-grow text-gray-600 outline-none rounded-l-lg font-medium" />
+    <button class="rounded-r-lg">
+        <img src="/src/assets/search.svg" class="w-7 h-7" alt="search" id="search-icon">
     </button>
 </div>
+<div id="dropdown" class="top-16 absolute w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+    <div class="py-1" role="none">
+      <!-- Active: "bg-gray-100 text-gray-900 outline-none", Not Active: "text-gray-700" -->
+      <button @click="updateSearchQuery(service.titolo)" v-for="service in filteredServices" v-if="searchQuery" :key="service._id" href="#" class="block px-4 py-2 text-sm text-gray-600" role="menuitem" tabindex="-1" id="menu-item-0">{{ service.titolo }}</button>
+    </div>
+  </div>
 </template>
 <script>
 export default{
-
+  data(){
+    return{
+      searchQuery: "", //input nella searchbar
+      services: [
+        {
+          _id: 1,
+          titolo: "La mia trento",
+          show: true
+        },
+        {
+          _id: 2,
+          titolo: "Trec+ cartella clinica",
+          show: true
+        },
+        {
+          _id:3,
+          titolo: "OnOff",
+          show: true
+        },
+        {
+          _id:4,
+          titolo: "Muoversi in Trentino",
+          show: true
+        },
+      ]
+    }
+  },
+  computed: {
+    filteredServices(){
+      return this.services.filter(service => service.show)
+    }
+  },
+  watch: {
+    // Osserva le modifiche a searchQuery e aggiorna i servizi
+    searchQuery() {
+      this.updateShow();
+    },
+  },
+  methods: {
+  // Aggiorna la proprietà `show` in base alla ricerca
+    updateShow() {
+      const query = this.searchQuery.toLowerCase();
+      this.services.forEach((service) => {
+        service.show = service.titolo.toLowerCase() === this.searchQuery.toLowerCase() ? false : service.titolo.toLowerCase().includes(query);
+      });
+      console.log(this.services);
+    },
+    updateSearchQuery(text){
+      this.searchQuery = text;
+    }
+  }
 }
 </script>
+<style scoped>
+#search-icon, #search-div, #search-input, #dropdown{
+  background-color: #F0F0F0;
+}
+#search-div{
+  margin-left: 25%;
+}
+#dropdown{
+  left: 39%;
+}
+
+</style>
