88377f985745d20840b1dee98090c19c2ca74b4d
Luca Da Riz <luca.dariz-1@studenti.unitn.it>
2024-11-28 13:46:44 +0100

    prova

--- Back-end/routes/segnalazioniRoutes.js
+++ Back-end/routes/segnalazioniRoutes.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-// le api (suppongo) vanno qui
+router.get('/', async (req, res) => {
+  try {
+    const avvisi = await Avviso.find({ servizio_id: new mongoose.Types.ObjectId(req.service_id) });
+    res.status(200).json(avvisi);
+  } catch (error) {
+    res.status(500).json({ message: 'Errore nel recupero dei dati', error: error.message });
+  }
+});
 
 module.exports = router;
