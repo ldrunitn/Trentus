89ea1d60e2348b068fd1c0b38381793a1935f7a5
Ricxel <giovannisbalchiero04ita@gmail.com>
2025-02-10 16:35:47 +0100

    Fix sidebar admin

--- frontend/src/components/admin/sidebarLeft/AdminSidebarLeft.vue
+++ frontend/src/components/admin/sidebarLeft/AdminSidebarLeft.vue
@@ -1,20 +1,5 @@
-<!-- <script setup>
-
-</script>
-
-<template> 
-    <div>
-        <div class="bg-gray-300 rounded-lg shadow-md mr-4 max-w-prose">
-            <div class="p-8">
-                <h2 class="text-xl font-bold">Notfiche</h2>
-                <p class="text-gray-600 bg">notifichenotifichenotifiche</p>
-            </div>
-        </div>
-    </div>
-</template> -->
-
 <template>
-  <div class="w-64 flex-col bg-gray-100 p-4 shadow-md mr-4 max-w-prose h-full">
+  <div class="w-64 flex-col bg-gray-100 p-4 mr-4 max-w-prose h-full">
     <h2 class="text-lg font-semibold mb-2">Richieste</h2>
     <RequestsContainer></RequestsContainer>
   </div>
